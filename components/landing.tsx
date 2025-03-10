afef4ed72b2a6b1fcdb5fbbff32af80df750db3b
M-Jey <mjeyict3@gmail.com>
2025-03-10 19:31:44 +0800

    Update landing page layout with additional buttons and improved styling

--- components/landing.tsx
+++ components/landing.tsx
@@ -9,16 +9,20 @@ export default function Landing() {
         source={require("../assets/bg.png")}
         className="flex-1 items-center justify-center"
       >
-        <View className="items-center">
+        <View className="flex h-[60vh] items-center justify-between">
           <Image
             source={require("../assets/logo.png")}
-            className="object-contain scale-[0.9]"
+            className="object-contain scale-[0.9] mt-[-60px]"
           />
 
-          <View>
-            <Pressable className="border-black border-[3px] w-[200px] h-[70px] rounded-[30px] items-center justify-center">
+          <View className="flex-col items-center mt-[100px]">
+            <Pressable className="z-20 top-[280px] border-black border-[3px] w-[250px] h-[60px] rounded-[20px] items-center justify-center">
               <Text className="text-2xl font-bold">Login</Text>
             </Pressable>
+            <Pressable className="z-10 top-[300px] border-black border-[3px] w-[200px] h-[60px] rounded-[20px] items-center justify-center">
+                <Text className="text-2xl font-bold">Use Local</Text>
+            </Pressable>
+            <Image source={require('../assets/gradient-b.png')} className="z-0 top-[2px]"/>
           </View>
           <StatusBar style="auto" />
         </View>
