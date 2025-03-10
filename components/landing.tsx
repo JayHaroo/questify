d905155c3780ee0f8528ebb506292422a74b352d
M-Jey <mjeyict3@gmail.com>
2025-03-10 19:11:34 +0800

    Initial Commit

---
+++ components/landing.tsx
@@ -0,0 +1,12 @@
+import { View, Text, ImageBackground, Image } from 'react-native';
+import 'nativewind'
+
+export default function Landing(){
+    return(
+        <>
+        <View className='flex-1 bg-black'>
+            
+        </View>
+        </>
+    )
+}
